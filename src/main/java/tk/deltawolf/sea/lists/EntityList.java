2b8f51e2ddc20df7d2c1787a624c6fdcf3b9c67d
Delta <XDelta@users.noreply.github.com>
2019-10-10 17:21:48 -0700

    Fixed entity spawn conditions

--- src/main/java/tk/deltawolf/sea/lists/EntityList.java
+++ src/main/java/tk/deltawolf/sea/lists/EntityList.java
@@ -1,10 +1,13 @@
 package tk.deltawolf.sea.lists;
 
-import net.minecraft.entity.EntityClassification;
-import net.minecraft.entity.EntitySpawnPlacementRegistry;
-import net.minecraft.entity.EntityType;
+import net.minecraft.entity.*;
+import net.minecraft.entity.passive.WaterMobEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorld;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.Biomes;
+import net.minecraft.world.dimension.DimensionType;
+import net.minecraft.world.gen.Heightmap;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -13,11 +16,23 @@ import tk.deltawolf.sea.Reference;
 import tk.deltawolf.sea.entity.passive.fish.HaddockEntity;
 import tk.deltawolf.sea.entity.passive.fish.SwampFeederEntity;
 
+import java.util.Random;
+
 @Mod.EventBusSubscriber(modid = Reference.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 @ObjectHolder(Reference.MOD_ID)
 public class EntityList {
-	public static EntityType<?> HADDOCK = EntityType.Builder.create(HaddockEntity::new, EntityClassification.WATER_CREATURE).size(0.2f,0.3f).build(Reference.MOD_ID + ":haddock").setRegistryName(Reference.MOD_ID, "haddock");
-	public static EntityType<?> SWAMPFEEDER = EntityType.Builder.create(SwampFeederEntity::new, EntityClassification.WATER_CREATURE).size(0.2f,0.3f).build(Reference.MOD_ID + ":swamp_feeder").setRegistryName(Reference.MOD_ID, "swamp_feeder");
+	public static EntityType<HaddockEntity> HADDOCK = createEntity(HaddockEntity::new, EntityClassification.WATER_CREATURE,"haddock",0.2f,0.3f, 20);
+	public static EntityType<SwampFeederEntity> SWAMPFEEDER = createEntity(SwampFeederEntity::new, EntityClassification.WATER_CREATURE, "swamp_feeder",0.2f,0.3f, 20);
+
+	private static <T extends Entity> EntityType<T> createEntity(EntityType.IFactory<T> f, EntityClassification entityClass, String name, float width, float height, int trackingRange) {
+		EntityType<T> entity = EntityType.Builder.create(f, entityClass)
+			.size(width, height)
+			.setTrackingRange(trackingRange)
+			.build(Reference.MOD_ID + ":" + name);
+
+		entity.setRegistryName(Reference.MOD_ID, name);
+		return entity;
+	}
 
 	@SubscribeEvent
 	public static void registerEntities(RegistryEvent.Register<EntityType<?>> event) {
@@ -31,7 +46,14 @@ public class EntityList {
 
 	public static void registerSpawns() {
 		registerEntitySpawn(HADDOCK, 10,2,5, Biomes.COLD_OCEAN, Biomes.DEEP_COLD_OCEAN, Biomes.DEEP_OCEAN);
-		registerEntitySpawn(SWAMPFEEDER, 10,1,2, Biomes.SWAMP, Biomes.SWAMP_HILLS);
+		registerEntitySpawn(SWAMPFEEDER, 10,1,2, Biomes.SWAMP, Biomes.SWAMP_HILLS); //Biomes.SWAMP, Biomes.SWAMP_HILLS
+
+		registerSpawnConditions();
+	}
+
+	private static void registerSpawnConditions () {
+		EntitySpawnPlacementRegistry.register(HADDOCK, EntitySpawnPlacementRegistry.PlacementType.IN_WATER, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, EntityList::deepOceanCondition);
+		EntitySpawnPlacementRegistry.register(SWAMPFEEDER, EntitySpawnPlacementRegistry.PlacementType.IN_WATER, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, EntityList::swampCondition);
 	}
 
 	public static void registerEntitySpawn(EntityType<?> entity, int weight, int minGroupCountIn, int maxGroupCountIn, Biome... biomes) {
@@ -41,4 +63,28 @@ public class EntityList {
 			}
 		}
 	}
+
+	//TODO
+	private static boolean deepOceanCondition(EntityType<? extends WaterMobEntity> entityType, IWorld world, SpawnReason spawnReason, BlockPos pos, Random random) {
+		if(world.getDimension().getType() != DimensionType.OVERWORLD) return false;
+		return true;
+	}
+
+	private static boolean swampCondition(EntityType<? extends WaterMobEntity> entityType, IWorld world, SpawnReason spawnReason, BlockPos pos, Random random) {
+		if(world.getDimension().getType() != DimensionType.OVERWORLD) return false;
+		return (pos.getY() >= 50 && pos.getY() <= 64);
+	}
+
+	private static boolean ravineCondition(EntityType<? extends WaterMobEntity> entityType, IWorld world, SpawnReason spawnReason, BlockPos pos, Random random) {
+		if(world.getDimension().getType() != DimensionType.OVERWORLD) return false;
+		return pos.getY() <= 30;
+	}
+	private static boolean netherCondition(EntityType<? extends MobEntity> entityType, IWorld world, SpawnReason spawnReason, BlockPos pos, Random random) {
+		if(world.getDimension().getType() != DimensionType.THE_NETHER) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
 }
