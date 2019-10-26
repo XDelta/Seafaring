ccc6e332ae8bf942ecf59a9cf58c63b380264802
Delta <XDelta@users.noreply.github.com>
2019-10-26 15:41:37 -0700

    Bump Version Info

--- src/main/java/tk/deltawolf/sea/Reference.java
+++ src/main/java/tk/deltawolf/sea/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 	public static final String MOD_ID = "sea";
 	public static final String LANG = "en_us";
 	public static final String NAME = "Seafaring";
-	public static final String VERSION = "0.1.45";
+	public static final String VERSION = "0.1.47";
 	public static final String FORGEVER = "28.1.34";
 	public static final String MCVER = "1.14.4"; //1.14.4
 }
