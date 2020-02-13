86a04265e4c52327df1f97fe19f5663478b61923
Delta <XDelta@users.noreply.github.com>
2020-02-12 19:42:55 -0800

    Block State Properties for sea stones

---
+++ src/main/java/tk/deltawolf/sea/state/BlockStateProperties.java
@@ -0,0 +1,9 @@
+package tk.deltawolf.sea.state;
+
+import net.minecraft.state.IntegerProperty;
+
+public class BlockStateProperties {
+	public static final IntegerProperty SIZE_1_2 = IntegerProperty.create("size", 1, 2);
+	public static final IntegerProperty SIZE_1_3 = IntegerProperty.create("size", 1, 3);
+	public static final IntegerProperty SIZE_1_4 = IntegerProperty.create("size", 1, 4);
+}
