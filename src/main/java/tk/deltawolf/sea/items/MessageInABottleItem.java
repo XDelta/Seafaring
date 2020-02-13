42ef35b563bc19f0383f1c2ff0c4fd30a4584966
Delta <XDelta@users.noreply.github.com>
2020-02-12 19:38:44 -0800

    Rename MessageInABottleItem, cleanup imports, add item drops

--- src/main/java/tk/deltawolf/sea/items/MessageInABottleItem.java
+++ src/main/java/tk/deltawolf/sea/items/MessageInABottleItem.java
@@ -5,24 +5,24 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
+import tk.deltawolf.sea.lists.ItemList;
 
 import javax.annotation.Nonnull;
 import java.util.List;
 import java.util.Random;
 
-public class ItemMessageInABottle extends Item {
+public class MessageInABottleItem extends Item {
 	private Random rand = new Random();
 
-	public ItemMessageInABottle(Properties properties) {
+	public MessageInABottleItem(Properties properties) {
 		super(properties);
 	}
 
@@ -32,20 +32,31 @@ public class ItemMessageInABottle extends Item {
 		ItemStack heldStack = player.getHeldItem(hand);
 		world.playSound(null, player.posX, player.posY, player.posZ, SoundEvents.BLOCK_GLASS_BREAK, SoundCategory.PLAYERS, 0.5F, 0.4F / (rand.nextFloat() * 0.4F + 0.8F));
 
-		int messageRoll = rand.nextInt(12) + 1; //1-9
+		int messageRoll = rand.nextInt(12) + 1; //1-13
+		int fishRoll = rand.nextInt(3); //0-3
 		//TODO in future make a scroll of paper/note that shows the message if there is one otherwise notify in chat to player
 		String message;
 		if (messageRoll == 0) {//shouldn't be 0
 			message = "The message crumbled apart in your hands";
-		} else if (messageRoll == 2) {
+		} else if (messageRoll == 2) {//give player a piece of paper
 			message = "sea.message" + messageRoll;
-			//give player a piece of paper
+			player.inventory.addItemStackToInventory(new ItemStack(Items.PAPER));
+
 		} else if (messageRoll == 4) {
 			message = "sea.message" + messageRoll;
-			//give player a fish
-		} else if (messageRoll == 5) {
+			switch (fishRoll) {
+				case 1:
+					player.inventory.addItemStackToInventory(new ItemStack(Items.TROPICAL_FISH));
+					break;
+				case 2:
+					player.inventory.addItemStackToInventory(new ItemStack(ItemList.haddock));
+					break;
+				default://0
+					player.inventory.addItemStackToInventory(new ItemStack(Items.COD));
+			}
+		} else if (messageRoll == 5) {//give player sea_moss
 			message = "sea.message" + messageRoll;
-			//give player sea_moss
+			player.inventory.addItemStackToInventory(new ItemStack(ItemList.sea_moss));
 		} else {
 			message = "sea.message" + messageRoll;
 		}
@@ -62,7 +73,6 @@ public class ItemMessageInABottle extends Item {
 
 	@Override
 	public void addInformation(ItemStack stack, World world, List<ITextComponent> list, ITooltipFlag flag) {
-		super.addInformation(stack, world, list, flag);
-		list.add(new StringTextComponent(TextFormatting.AQUA + "Well, it's a bottle with a message inside"));
+		list.add(new TranslationTextComponent("item.sea.message_in_a_bottle.tooltip"));
 	}
 }
